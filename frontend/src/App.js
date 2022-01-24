36ad6879fdfca4a80a88d2eadf16dbc0e9cf7991
Ahmad Allouch <50431754+ahmadallouch@users.noreply.github.com>
2022-01-23 19:48:02 -0500

    Create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Course Goals</h1>
+      <ul>
+        <li>Finish the Course</li>
+        <li>To create a cool appfffdgfdasgagdg</li>
+        <li>To create a cool appfffdgfdasgagdg</li>
+        <li>To create a cool appfffdgfdasgagdg</li>
+        <li>To create a cool appfffdgfdasgagdg</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
